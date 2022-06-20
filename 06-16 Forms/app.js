bba9483f047450e2607fb974f411267a8174dd1a
Ernestas Ragainis <75485540+grietinetis@users.noreply.github.com>
2022-06-20 17:04:50 +0300

    new Tasks

--- 06-16 Forms/app.js
+++ 06-16 Forms/app.js
@@ -1,156 +1,216 @@
+const studentForm = document.querySelector('form');
+
+    const INITIAL_STUDENT_DATA = [
+        {
+            name: 'Vardas1',
+            surname: 'Pavarde1',
+            age: 30,
+            phone: '3701241221',
+            email: 'asdasd@gmail.com',
+            group: 'type11',
+            itKnowledge: 3,
+            interests: ['Python', 'C++'] 
+        },
+        {
+            name: 'Vardas2',
+            surname: 'Pavarde2',
+            age: 25,
+            phone: '37016341221',
+            email: 'arbuzas@gmail.com',
+            group: 'type11',    
+            itKnowledge: 7,    
+            interests: ['Javascript', 'C#'] 
+        },
+        {
+            name: 'Vardas3',
+            surname: 'Pavarde3',
+            age: 21,
+            phone: '370222341221',
+            email: 'melionas@gmail.com',
+            group: 'type11',
+            itKnowledge: 5,  
+            interests: ['Ruby', 'C++'] 
+        },
+        {
+            name: 'Vardas4',
+            surname: 'Pavarde4',
+            age: 26,
+            phone: '370666341221',
+            email: 'bananas@gmail.com',
+            group: 'type11',
+            itKnowledge: 1,  
+            interests: ['Javascript', 'Rust'] 
+        },
+        {
+            name: 'Vardas5',
+            surname: 'Pavarde5',
+            age: 22,
+            phone: '370111341221',
+            email: 'agurkas@gmail.com',
+            group: 'type11',
+            itKnowledge: 2, 
+            interests: ['Javascript', 'C++'] 
+        },
+
+    ]
+
+    function renderInitialData(students) {
+        students.map((student)=>{
+            let studentName = student.name
+            let studentSurname = student.surname
+            // let studentAge = studentForm.querySelector('#student-age').value;
+            let studentAge = student.age
+            let studentPhone = student.phone
+            let studentEmail = student.email
+            let studentItKnowledge = student.itKnowledge
+            let studentGroup = student.group
+            let interests = student.interest
+        })
+    }   
 
     
-    
-    const studentForm = document.querySelector('form');
-
-    const itKnowledgeInputElement = document.querySelector('#student-it-knowledge')
-    const itKnowledgeOutputElement = document.querySelector('#it-knowledge-output')
-
-    itKnowledgeInputElement.addEventListener('input',(event) => {
-        itKnowledgeOutputElement.textContent = event.target.value
-        
-    }) 
-
-    studentForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    let studentName = document.querySelector('#student-name').value;
-    let studentSurname = document.getElementById('student-surname').value;
-    // let studentAge = studentForm.querySelector('#student-age').value;
-    let studentAge = event.target.querySelector('#student-age').value;
-    let studentPhone = studentForm.querySelector('[name="phone"]').value;
-    let studentEmail = event.target.elements.email.value;
-    let studentItKnowledge = event.target.elements['it-knowledge'].value
-    let studentGroup = event.target.elements.group.value
-    let interests = document.querySelectorAll('input[name="interest"]:checked')
-   
-    let errorMessage = document.createElement('span')
-    errorMessage.textContent = ' Sis laukelis yra privalomas'
 
 
-    let studentsList = document.querySelector('#students-list')
-    let studentItem = document.createElement('div')
 
-    if(!studentName) {
-        document.querySelector('#student-name').style.borderColor = 'red'
-        document.querySelector('#student-name').after(errorMessage)
-        return;
+const itKnowledgeInputElement = document.querySelector('#student-it-knowledge');
+const itKnowledgeOutputElement = document.querySelector('#it-knowledge-output');
+itKnowledgeInputElement.addEventListener('input', (event) => {
+  // console.log(itKnowledgeInputElement.value)
+  // console.log(event.target.value);
+  itKnowledgeOutputElement.textContent = event.target.value;
+});
+studentForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  let studentName = document.querySelector('#student-name').value;
+  let studentSurname = document.getElementById('student-surname').value;
+  // let studentAge = studentForm.querySelector('#student-age').value;
+  let studentAge = event.target.querySelector('#student-age').value;
+  let studentPhone = studentForm.querySelector('[name="phone"]').value;
+  let studentEmail = event.target.elements.email.value;
+  let studentItKnowledge = event.target.elements['it-knowledge'].value;
+  // let studentGroup = document.querySelector('input[name="group"]:checked');
+  let studentGroup = event.target.elements.group.value;
+  let interests = document.querySelectorAll('input[name="interest"]:checked');
+  // let studentNameInput = document.querySelector('#student-name');
+  // studentNameInput.style.borderColor = '';
+  // let inputErrorMessage = document.querySelector('.input-error-message');
+  // if (inputErrorMessage) {
+  //   inputErrorMessage.remove();
+  // }
+  // if (!studentName) {
+  //   let alertText = 'Ne visi laukeliai užpildyti.';
+  //   alertMessage(alertText, 'error-alert');
+  //   studentNameInput.style.borderColor = 'red';
+  //   let inputError = document.createElement('span');
+  //   inputError.textContent = 'Šis laukelis yra privalomas';
+  //   inputError.classList.add('input-error-message');
+  //   studentNameInput.after(inputError);
+  //   return;
+  // }
+  document.querySelectorAll('.input-error-message').forEach(input => input.remove());
+  let requiredInputs = document.querySelectorAll('input.required');
+  let validForm = true;
+  requiredInputs.forEach(input => {
+    input.classList.remove('input-error');
+    if (!input.value) {
+      validForm = false;
+      let alertText = 'Ne visi laukeliai užpildyti.';
+      alertMessage(alertText, 'error-alert');
+      input.classList.add('input-error');
+      let inputError = document.createElement('span');
+      inputError.textContent = 'Šis laukelis yra privalomas';
+      inputError.classList.add('input-error-message');
+      input.after(inputError);
     }
+  })
+  if (!validForm) {
+    return;
+  }
+  let studentsList = document.querySelector('#students-list');
+  let studentItem = document.createElement('div');
+  studentItem.classList.add('student-item');
+  let nameElement = document.createElement('p');
+  nameElement.innerHTML = `<strong>Name:</strong> ${renderInitialData(INITIAL_STUDENT_DATA)}`;
+  let surnameElement = document.createElement('p');
+  surnameElement.innerHTML = `<strong>Surname:</strong> ${studentSurname}`;
+  let ageElement = document.createElement('p');
+  ageElement.innerHTML = `<strong>Age:</strong> ${studentAge}`;
+  let phoneElement = document.createElement('p');
+  // phoneElement.innerHTML = `<strong>Phone:</strong> ${studentPhone}`;
+  phoneElement.innerHTML = `<strong>Phone:</strong> ****`;
+  let emailElement = document.createElement('p');
+  // emailElement.innerHTML = `<strong>Email:</strong> ${studentEmail}`;
+  emailElement.innerHTML = `<strong>Email:</strong> ****`;
+  let itKnowledgeElement = document.createElement('p');
+  itKnowledgeElement.innerHTML = `<strong>IT Knowledge:</strong> ${studentItKnowledge}`;
+  let groupElement = document.createElement('p');
+  groupElement.innerHTML = `<strong>Group:</strong> ${studentGroup}`;
+  let interestWrapperElement = document.createElement('div');
+  interestWrapperElement.classList.add('interest-wrapper');
+  let interestTitleElement = document.createElement('h4');
+  interestTitleElement.classList.add('interest-title');
+  interestTitleElement.textContent = 'Interests:';
+  let interestListElement = document.createElement('ul');
+  interestListElement.classList.add('interest-list');
+  interests.forEach(interest => {
+    let interestItemElement = document.createElement('li');
+    interestItemElement.textContent = interest.value;
+    interestListElement.append(interestItemElement);
+  });
 
-
-
-            studentItem.classList.add('student-item')
-
-            let nameElement = document.createElement('p')
-            nameElement.textContent = `name: ${studentName}`
-            
-
-            let surnameElement = document.createElement('p')
-            surnameElement.textContent = `Surname: ${studentSurname}`
-    
-            let ageElement = document.createElement('p')
-            ageElement.textContent  = `Age: ${studentAge}`
-            
-            let phoneElement = document.createElement('p')
-            phoneElement.textContent  = `Phone: *****`
-     
-            let emailElement = document.createElement('p')
-            emailElement.textContent  = `Email: *****`
-
-            let itKnowledgeElement = document.createElement('p')
-            itKnowledgeElement.textContent  = `Knowledge: ${studentItKnowledge}`
-
-            let groupElement = document.createElement('p')
-            groupElement.textContent  = `group: ${studentGroup}`
-
-            let interestWrapperElement = document.createElement('div')
-            interestWrapperElement.classList.add('interest-wrapper')
-        
-            let interestTitleElement = document.createElement('h4')
-            interestTitleElement.classList.add('interest-title')
-            interestTitleElement.textContent = 'Interests:'
-
-            let interestListElement = document.createElement('ul')
-            interestListElement.classList.add('interest-list')
-
-            interests.forEach(interest => {
-                let interestItemElement = document.createElement('li')
-                interestItemElement.textContent = interest.value
-                interestListElement.append(interestItemElement)
-            })
-
-            let privateInfoButton = document.createElement('button')
-            privateInfoButton.textContent = 'Rodyti Info'
-
-            let hiddenData = true
-
-
-            privateInfoButton.addEventListener('click' ,() => {
-                if(hiddenData) {
-                    phoneElement.innerHTML = `Phone: ${studentPhone}`
-                    emailElement.innerHTML = `Email: ${studentEmail}`
-                    privateInfoButton.textContent = 'Slepti'
-                    hiddenData = false;
-                } else {
-                    phoneElement.innerHTML = `Phone: ***`
-                    emailElement.innerHTML = `Email: ***`
-                    privateInfoButton.textContent = 'Rodyti'
-                    hiddenData = true;
-                }
-
-                
-
-            })
-
-
-            interestWrapperElement.append(interestTitleElement,interestListElement)
-            studentItem.append(nameElement,surnameElement,ageElement,phoneElement,emailElement,itKnowledgeElement,groupElement, interestWrapperElement,privateInfoButton)
-            studentsList.prepend(studentItem)
-        
-
-
-            // studentForm.reset()
-            event.target.reset()
-
-            const alertElement = document.querySelector('#alert')
-            let alertText = `Sukurtas studentas ${studentName} ${studentSurname}`;
-
-            alertElement.textContent = alertText
-
-            setTimeout(() => {
-                alertElement.textContent = ''
-            }, 5000);
-    })
-    
-//     4. Formoje pridėti „checkbox" tipo input'ą, kuriame pateikta galimybę rinktis iš dominančių programavimo kalbų.
-// 5. Dominančias programavimo kalbas atvaizduoti „student-item" elemente.
-
-// 6. Sukūrus studentą, turi iššokti <span> elementas, kuris informuoja apie studento sukūrimą: „Sukurtas studentas (Vardas Pavardė)". Šis span elementas dingsta po 5 sekundžių.
-
-// 7. Range reikšmės atvaizdavimas naujame elemente.
-    
-  
-  
-
-// TREČIA DALIS:
-// 1. Vietoje asmens duomenų (el. paštas ir tel. nr) rodyti tik žvaigždutes „****".
-// 2. Kiekviename „student-item" elemente sukurti mygtuką „Rodyti asmens duomenis".
-// 3. Paspaudus šį mygtuką:
-// 3.1. Parodyti to studento el. paštą ir tel. nr.
-// 3.2. Pakeist mygtuko tekstą į „Slėpti asmens duomenis".
-// 4. Jeigu asmens duomenys yra rodomi, tai paspaudus mygtuką:
-// 4.1. Paslėpti asmens duomenis.
-// 4.2. Mygtuko tekstą pakeisti į „Rodyti asmens duomenis".
-
-
-
-
-// Papildyti formos validaciją. Jeigu:
-// 1. Vardas yra trumpesnis nei 3 simboliai, parašyti: „Vardas privalo būti bent 3 simbolių ilgumo".
-// 2. Pavardė yra trumpesnė nei 3 simboliai, parašyti: „Pavardė privalo būti bent 3 simbolių ilgumo".
-// 3. Amžius yra neigamas, parašyti: „Amžius privalo būti teigiamas skaičius".
-// 4. Amžius yra daugiau nei 120 metų, parašyti: „Įvestas amžius yra per didelis".
-// 5. Telefono numeris yra mažiau nei 9 arba daugiau nei 12, parašyti: „Įvestas telefono numeris yra neteisingas".
-// 6. Elektroninis paštas yra trumpesnis nei 5 simboliai arba jame nėra panaudotas @ simbolis, parašyti: „Įvestas elektroninis paštas yra neteisingas".
-
-
-
+  interestWrapperElement.append(interestTitleElement, interestListElement);
+  let privateInfoButton = document.createElement('button');
+  privateInfoButton.textContent = 'Rodyti asmens duomenis';
+  // let hiddenData = true;
+  // privateInfoButton.addEventListener('click', () => {
+  //   if (hiddenData) {
+  //     phoneElement.innerHTML = `<strong>Phone:</strong> ${studentPhone}`;
+  //     emailElement.innerHTML = `<strong>Email:</strong> ${studentEmail}`;
+  //     privateInfoButton.textContent = 'Slėpti asmens duomenis';
+  //   } else {      
+  //     phoneElement.innerHTML = `<strong>Phone:</strong> ****`;
+  //     emailElement.innerHTML = `<strong>Email:</strong> ****`;
+  //     privateInfoButton.textContent = 'Rodyti asmens duomenis';
+  //   }
+  //   hiddenData = !hiddenData;
+  // });
+  privateInfoButton.addEventListener('click', () => {
+    if (!privateInfoButton.classList.contains('hide')) {
+      phoneElement.innerHTML = `<strong>Phone:</strong> ${studentPhone}`;
+      emailElement.innerHTML = `<strong>Email:</strong> ${studentEmail}`;
+      privateInfoButton.textContent = 'Slėpti asmens duomenis';
+    } else {      
+      phoneElement.innerHTML = `<strong>Phone:</strong> ****`;
+      emailElement.innerHTML = `<strong>Email:</strong> ****`;
+      privateInfoButton.textContent = 'Rodyti asmens duomenis';
+    }
+    privateInfoButton.classList.toggle('hide');
+  });
+  let deleteStudentButton = document.createElement('button');
+  deleteStudentButton.textContent = 'Remove student';
+  deleteStudentButton.addEventListener('click', () => {
+    studentItem.remove();
+    let messageText = `Student deleted (${studentName} ${studentSurname})`;
+    alertMessage(messageText);
+  })
+  studentItem.append(nameElement, surnameElement, ageElement, phoneElement, emailElement, itKnowledgeElement, groupElement, interestWrapperElement, privateInfoButton, deleteStudentButton);
+  studentsList.prepend(studentItem);
+  // studentForm.reset();
+  event.target.reset();
+  let alertText = `Student created (${studentName} ${studentSurname})`;
+  alertMessage(alertText);
+});
+function alertMessage(text, elementClass = '') {
+  const alertElement = document.querySelector('#alert');
+  alertElement.textContent = text;
+  if (elementClass) {
+    alertElement.classList.add(elementClass);
+  }
+  setTimeout(() => {
+    alertElement.textContent = '';
+    if (elementClass) {
+      alertElement.classList.remove(elementClass);
+    }
+  }, 5000);
+}
